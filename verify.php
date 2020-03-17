875fa4f6007b76f2d82331bca98f69780ecfd3f9
Abolfazll Poorali <abolfazll.poorali@gmail.com>
2020-03-17 11:03:51 +0330

    Fix pasargad bill pays

--- verify.php
+++ verify.php
@@ -26,17 +26,27 @@
 					$transactionType = explode('-', $result['products']['type']);
 					$transactionType = $transactionType[0];
 					if ($transactionType == 'Bill') {
-						$billTypesPersian = array("آب", "بــرق", "گـــاز", "تلفن ثابت", "تلفن همراه", "عوارض شهرداری","","","جریمه راهنمایی و رانندگی");
-						$billTypesEnglish = array("water", "electricity", "gas", "telephone", "cellphone", "mayoralty","","","police");
-			?>
+                        $billTypesPersian = array("آب", "بــرق", "گـــاز", "تلفن ثابت", "تلفن همراه", "عوارض شهرداری","","", "جریمه راهنمایی و رانندگی","بیمه پاسارگاد","سایر");
+                        $billTypesEnglish = array("water", "electricity", "gas", "telephone", "cellphone", "mayoralty","","", "police","pasargad","others");
+                        $billType = $result['products']['details']['billType'] - 1;
+                        if($billType == -1){
+                            $billCoNum = substr($result['products']['details']['billId'],strlen($result['products']['details']['billId'])-5,3);
+                            if($billCoNum == 102){
+                                $billType = 9;
+                            }else{
+                                $billType = 10;
+                            }
+                        }
+
+                        ?>
 						<div id="content" class="Bill">
 							<table id="bill-info">
 								<tbody>
 									<tr>
 										<td>نوع قبض</td>
 										<td>
-											<span id="type" class="bill <?php echo $billTypesEnglish[$result['products']['details']['billType'] - 1]; ?>"></span>
-											<span id="type-title"><?php echo $billTypesPersian[$result['products']['details']['billType'] - 1]; ?></span>
+                                            <span id="type" class="bill <?php echo $billTypesEnglish[$billType]; ?>"></span>
+                                            <span id="type-title"><?php echo $billTypesPersian[$billType]; ?></span>													</td>
 										</td>
 									</tr>
 									<tr>
